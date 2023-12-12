550859e8fee8e0168895b39d6438d8ff9ee4d07f
void57 <153325776+void57@users.noreply.github.com>
2023-12-12 05:59:57 +0530

    Create day8.js

---
+++ day8.js
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Weather</title>
+  </head>
+  <body>
+    <script>
+      const apiKey = "7466cf52719269e6e37fa5b562c29194";
+      const city = "Kolkata";
+
+      fetch(
+        `https://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=${apiKey}`
+      )
+        .then((res) => res.json())
+        .then((data) => {
+          console.log(data);
+        })
+        .catch((error) => {
+          console.log("Error fetching weather data:", error);
+        });
+    </script>
+  </body>
+</html>
